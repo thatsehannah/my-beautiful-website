cc25c23cefaecba4d537d13fba2af8568b418d62
E. Hannah <echannah631@gmail.com>
2025-05-31 10:59:37 -0400

    working on hero section, getting the video background to switch when clicking on small video player

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
+import Hero from "@/components/Hero";
 import React from "react";
 
 const page = () => {
   return (
-    <main>
-      <h1 className='text-5xl font-bold text-blue-300'>
-        Welcome to my Beautiful Website
-      </h1>
+    <main className='relative min-h-screen w-screen overflow-x-hidden'>
+      <Hero />
     </main>
   );
 };
---
+++ components/Hero.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import React, { useRef, useState } from "react";
+
+const Hero = () => {
+  const [currentIndex, setCurrentIndex] = useState(1);
+  const [hasClicked, setHasClicked] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [loadedVideos, setLoadedVideos] = useState(0);
+
+  const totalVideos = 3;
+  const nextVideoRef = useRef(null);
+
+  const upcomingVideoIndex = (currentIndex % totalVideos) + 1;
+
+  const handleMiniVideoClick = () => {
+    setHasClicked(true);
+    setCurrentIndex(upcomingVideoIndex);
+  };
+
+  const handleVideoLoad = () => {
+    setLoadedVideos((prev) => prev + 1);
+  };
+
+  const getVideoSrc = (index: number) => `videos/hero-${index}.mp4`;
+
+  return (
+    <div className='relative h-dvh w-screen overflow-x-hidden'>
+      <div
+        id='video-frame'
+        className='relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75'
+      >
+        <div>
+          <div className='mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg'>
+            <div
+              onClick={handleMiniVideoClick}
+              className='origin-center scale-50 opacity-0 transition-all duration-500 ease-in hover:scale-100 hover:opacity-100'
+            >
+              <video
+                src={getVideoSrc(upcomingVideoIndex)}
+                ref={nextVideoRef}
+                loop
+                muted
+                id='current-video'
+                className='size-64 origin-center scale-150 object-cover object-center'
+                onLoadedData={handleVideoLoad}
+              />
+            </div>
+          </div>
+          <video
+            ref={nextVideoRef}
+            src={getVideoSrc(currentIndex)}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
