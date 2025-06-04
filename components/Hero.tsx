34c7c3e165e0ca0a4363e3a831683443e7142e13
E. Hannah <echannah631@gmail.com>
2025-06-04 19:54:54 -0400

    making adjustments to hero section for when videos are loading, adding loading div for when app is loading, adding in more css from tutorial css

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,10 +1,13 @@
 "use client";
 
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Button from "./Button";
 import { TiLocationArrow } from "react-icons/ti";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const Hero = () => {
   const [currentIndex, setCurrentIndex] = useState(1);
@@ -12,8 +15,9 @@ const Hero = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [loadedVideos, setLoadedVideos] = useState(0);
 
-  const totalVideos = 3;
+  const totalVideos = 4;
   const nextVideoRef = useRef<HTMLVideoElement>(null);
+  const loadedVideoSet = useRef<Set<HTMLVideoElement>>(new Set());
 
   const upcomingVideoIndex = (currentIndex % totalVideos) + 1;
 
@@ -22,9 +26,42 @@ const Hero = () => {
     setCurrentIndex(upcomingVideoIndex);
   };
 
-  const handleVideoLoad = () => {
-    setLoadedVideos((prev) => prev + 1);
+  const handleVideoLoad = (video?: HTMLVideoElement) => {
+    if (video && !loadedVideoSet.current.has(video)) {
+      loadedVideoSet.current.add(video);
+      setLoadedVideos((prev) => prev + 1);
+    }
   };
+
+  useEffect(() => {
+    const videos = document.querySelectorAll("video");
+
+    videos.forEach((video) => {
+      if (video.readyState >= 3) {
+        handleVideoLoad(video);
+      } else {
+        video.addEventListener("loadeddata", () => handleVideoLoad(video), {
+          once: true,
+        });
+      }
+    });
+
+    return () => {
+      videos.forEach((video) => {
+        video.removeEventListener("loadeddata", () => {});
+      });
+    };
+  });
+
+  useEffect(() => {
+    console.log("Loaded videos count:", loadedVideos);
+
+    if (loadedVideos === totalVideos - 1) {
+      console.log("All videos loaded my guy.");
+
+      setIsLoading(false);
+    }
+  }, [loadedVideos]);
 
   //animation for changing the background video
   useGSAP(
@@ -58,7 +95,7 @@ const Hero = () => {
   //animation for scrolling past the hero section
   useGSAP(() => {
     gsap.set("#video-frame", {
-      clipPath: "polygon(14% 0%, 72% 0%, 90% 90%, 0% 100%)",
+      clipPath: "polygon(14% 0, 72% 0, 88% 90%, 0 95%)",
       borderRadius: "0 0 40% 10%",
     });
     gsap.from("#video-frame", {
@@ -78,6 +115,15 @@ const Hero = () => {
 
   return (
     <div className='relative h-dvh w-screen overflow-x-hidden'>
+      {isLoading && (
+        <div className='flex-center absolute z-[100] h-dvh w-screen overflow-hidden bg-violet-50'>
+          <div className='three-body'>
+            <div className='three-body__dot' />
+            <div className='three-body__dot' />
+            <div className='three-body__dot' />
+          </div>
+        </div>
+      )}
       <div
         id='video-frame'
         className='relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75'
@@ -95,7 +141,6 @@ const Hero = () => {
                 muted
                 id='current-video'
                 className='size-64 origin-center scale-150 object-cover object-center'
-                onLoadedData={handleVideoLoad}
               />
             </div>
           </div>
@@ -106,7 +151,6 @@ const Hero = () => {
             muted
             id='next-video'
             className='absolute-center invisible absolute z-20 size-64 object-cover object-center'
-            onLoadedData={handleVideoLoad}
           />
           <video
             src={getVideoSrc(
@@ -116,7 +160,6 @@ const Hero = () => {
             loop
             muted
             className='absolute left-0 top-0 size-full object-cover object-center'
-            onLoadedData={handleVideoLoad}
           />
         </div>
         <h1 className='hero-heading absolute bottom-5 right-5 z-40 text-blue-75'>
