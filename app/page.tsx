cef5a3d7bb9540c5d035ffd6db1b127acef78310
E. Hannah <echannah631@gmail.com>
2025-06-05 00:40:06 -0400

    creating the nav bar

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import About from "@/components/About";
 import Hero from "@/components/Hero";
+import Navbar from "@/components/Navbar";
 import React from "react";
 
 const page = () => {
   return (
     <main className='relative min-h-screen w-screen overflow-x-hidden'>
+      <Navbar />
       <Hero />
       <About />
     </main>
