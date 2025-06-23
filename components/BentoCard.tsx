9331d0471d4b2538aa4b22952f265b741712c00d
E. Hannah <echannah631@gmail.com>
2025-06-23 15:32:17 -0400

    creating bento grid and bento card component

---
+++ components/BentoCard.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+type BentoCardProps = {
+  src: string;
+  title: string;
+  description?: string;
+};
+
+const BentoCard = ({ src, title, description }: BentoCardProps) => {
+  return (
+    <div className='relative size-full'>
+      <video
+        src={src}
+        loop
+        muted
+        autoPlay
+        className='absolute left-0 top-0 size-full object-cover object-center'
+      />
+      <div className='relative z-10 flex size-full flex-col justify-between p-5 text-blue-50'>
+        <div>
+          <h1 className='bento-title'>{title}</h1>
+          {description && (
+            <p className='mt-3 max-w-64 text-xs md:text-base'>{description}</p>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BentoCard;
--- components/Features.tsx
+++ components/Features.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import BentoCard from "./BentoCard";
+import { TiLocationArrow } from "react-icons/ti";
 
 const Features = () => {
   return (
@@ -14,7 +16,53 @@ const Features = () => {
             on your world.
           </p>
         </div>
-        <div className='border-hsla relative mb-7 h-96 w-full overflow-hidden rounded-md md:h-65vh'></div>
+        <div className='border-hsla relative mb-7 h-96 w-full overflow-hidden rounded-md md:h-65vh'>
+          <BentoCard
+            src='videos/feature-1.mp4'
+            title='radiant'
+            description='A cross-platform metagame app, turning your activities across Web2 and Web3 games into a rewarding adventure.'
+          />
+        </div>
+        <div className='grid h-[135vh] grid-cols-2 grid-rows-3 gap-7'>
+          <div className='bento-tilt_1 row-span-1 md:col-span-1 md:row-span-2'>
+            <BentoCard
+              src='videos/feature-2.mp4'
+              title='zigma'
+              description='An anime and gaming-inspired NFT collection - the IP primed for expansion.'
+            />
+          </div>
+          <div className='bento-tilt_1 row-span-1 ms-32 md:col-span-1 md:ms-0'>
+            <BentoCard
+              src='videos/feature-3.mp4'
+              title='nexus'
+              description='A gamified social hub, adding a new dimension of play to social interaction for Web3 communities.'
+            />
+          </div>
+          <div className='bento-tilt_1 me-14 md:col-span-1 md:me-0'>
+            <BentoCard
+              src='videos/feature-4.mp4'
+              title='azul'
+              description='A cross-world AI Agent - elevating your gameplay to be more fun and productive.'
+            />
+          </div>
+          <div className='bento-tilt_2'>
+            <div className='flex size-full flex-col justify-between bg-violet-300 p-5'>
+              <h1 className='bento-title max-w-64 text-black'>
+                More coming soon!
+              </h1>
+              <TiLocationArrow className='m-5 scale-[5] self-end' />
+            </div>
+          </div>
+          <div className='bento-tilt_2'>
+            <video
+              src='videos/feature-5.mp4'
+              loop
+              muted
+              autoPlay
+              className='size-full object-cover object-center'
+            />
+          </div>
+        </div>
       </div>
     </section>
   );
